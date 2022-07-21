f72153fba7140367558a57b83e2117b292f28f9b
Jerry Chang <therealjerrycan@gmail.com>
2022-07-21 00:24:37 -0700

    Update config

--- functions/ingestion/src/config.ts
+++ functions/ingestion/src/config.ts
@@ -5,19 +5,22 @@ export default {
     enabled: Boolean(process.env.LOG_ENABLED) || false,
     redact: process.env.LOG_REDACT?.split(',') ?? [],
   },
-
   // Application level
+
+  // configuration for AWS SQS
   queue: {
     sqs: {
       url: process?.env?.QueueUrl ?? '',
       defaultRegion: process.env.DefaultRegion ?? 'us-east-1',
     }
   },
+
+  // configuration for the custom webhook
   webhook: {
     signature: {
-      secret: process.env.WEBHOOK_SIGNATURE_SECRET ?? '',
-      algo: process.env.WEBHOOK_SIGNATURE_ALGO ?? 'sha256',
-      header: process.env.WEBHOOK_SIGNATURE_HEADER ?? 'x-hub-signature-256',
+      secret: process.env.WebhookSignatureSecret ?? '',
+      algo: process.env.WebhookSignatureAlgo ?? 'sha256',
+      header: process.env.WebhookSignatureHeader ?? 'x-hub-signature-256',
     }
   }
 };
